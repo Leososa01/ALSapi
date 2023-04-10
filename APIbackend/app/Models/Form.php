2f4bd2eb315386615f8efa7743516dffc0ae1003
leososa01 <leo.sosa.96@hotmail.com>
2023-04-10 10:51:48 -0300

    mail y base de datos

--- APIbackend/app/Models/Form.php
+++ APIbackend/app/Models/Form.php
@@ -4,8 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+//tablas en la base de datos
 class Form extends Model
 {
     use HasFactory;
-}
+    protected $table = 'forms';
+    public $fillable =['nombre', 'email', 'edad', 'contrasena', 'contrasena_confirmation'];
+}  
